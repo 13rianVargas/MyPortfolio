975bf052d509cd31da8cd636d906fa8a0859ff11
13rian Vargas <bsvargasc13@gmail.com>
2025-06-22 23:57:16 -0500

    [FEAT][i18n] First part of the implementation of i18n
    I'm trying hard T-T

---
+++ src/i18n/ui.ts
@@ -0,0 +1,52 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+};
+
+export const defaultLang = 'es';
+export const showDefaultLang = false;
+
+export const ui = {
+    es: {
+        'head.title': "Portafolio | 13rian",
+        'layout.home': "Inicio",
+        'layout.about': "Sobre Mí",
+        'layout.projects': "Proyectos",
+        'layout.technologies': "Tecnologías",
+        'layout.contact': "Contáctame",
+        'layout.footer': "Hecho por 13rian Vargas",
+        'home.name': "Brian Vargas",
+        'home.role': "TODO Aquí va mi rol",
+        'aboutme.title': "Sobre Mí",
+        'aboutme.description': "TODO Aquí va una breve descripción sobre mí.",
+        'technologies.title': "Tecnologías",
+        'contactme.title': "Contáctame",
+        'contactme.name': "Nombre",
+        'contactme.email': "Correo electrónico",
+        'contactme.message': "Mensaje",
+        'contactme.send': "Enviar",
+        'projects.title': "Proyectos",
+        'projects.description': "TODO Aquí va una breve descripción de mis proyectos.",
+    },
+    en: {
+        'head.title': "Portfolio | 13rian",
+        'layout.home': "Home",
+        'layout.about': "About Me",
+        'layout.projects': "Projects",
+        'layout.technologies': "Technologies",
+        'layout.contact': "Contact Me",
+        'layout.footer': "Made by 13rian Vargas",
+        'home.name': "Brian Vargas",
+        'home.role': "TODO Here goes my role",
+        'aboutme.title': "About Me",
+        'aboutme.description': "TODO Here goes a brief description about me.",
+        'technologies.title': "Technologies",
+        'contactme.title': "Contact Me",
+        'contactme.name': "Name",
+        'contactme.email': "Email",
+        'contactme.message': "Message",
+        'contactme.send': "Send",
+        'projects.title': "Projects",
+        'projects.description': "TODO Here goes a brief description of my projects.",
+    },
+} as const;
